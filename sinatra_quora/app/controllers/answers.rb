8834bc3553b5977d1330832a350d768726b26637
codeamac01 <codeamac01@gmail.com>
2016-08-12 18:09:01 -0500

    se añadió base de CRUD(revisar)

---
+++ sinatra_quora/app/controllers/answers.rb
@@ -0,0 +1,27 @@
+#CREATE
+get '/answer/new' do
+  
+end
+
+post '/new_answer' do
+
+end
+
+#READ
+get '/answer/:id' do
+
+end
+
+#UPDATE
+get '/answer/:id/edit' do
+
+end
+
+put '/answer/:id' do
+
+end
+
+#DELETE
+delete '/answer/:id' do
+
+end
